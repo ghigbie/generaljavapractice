0a6bcf3e0408d7bc4d487963bc747a2b1167253f
ghigbie <gehigbie@gmail.com>
2020-01-02 20:10:30 -0700

    Add new class

--- src/com/higbie/classes/ExampleIO.java
+++ src/com/higbie/classes/ExampleIO.java
@@ -1,13 +1,24 @@
 package com.higbie.classes;
 
+import java.util.Scanner;
+
 public class ExampleIO {
 
-    public static void main(Sring[] args){
+    public static void main(String[] args){
         int result = divide();
         System.out.println(result);
     }
 
     private static int divide(){
-        int x =
+        int x = getInt();
+        int y = getInt();
+        System.out.println("X is " + x + ", Y is "+ y);
+        return x/y;
+    }
+
+    private static int getInt(){
+        Scanner s = new Scanner(System.in);
+        System.out.println("Please enter an integer ");
+        return s.nextInt();
     }
 }
